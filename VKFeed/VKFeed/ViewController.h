a81174c2293f687fbc73737a1d38911de19aaa95
Daniil Novoselov <Dan2803@yandex.ru>
2016-05-08 11:45:04 +0300

    - pod setup

---
+++ VKFeed/VKFeed/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VKFeed
+//
+//  Created by Daniil Novoselov on 07.05.16.
+//  Copyright © 2016 Daniil Novoselov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VKFeed/VKFeedTests/VKFeedTests.m
@@ -0,0 +1,39 @@
+//
+//  VKFeedTests.m
+//  VKFeedTests
+//
+//  Created by Daniil Novoselov on 07.05.16.
+//  Copyright © 2016 Daniil Novoselov. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface VKFeedTests : XCTestCase
+
+@end
+
+@implementation VKFeedTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
